001f3c07b01810d570168f76812c8443d7fcc67b
1439293823@qq.com <hzoschina525672>
2018-04-27 19:56:41 +0800

    checklink

--- js/validator.js
+++ js/validator.js
@@ -41,3 +41,21 @@ function isZipCode(str){var patrn=/^\d{6}$/;return patrn.test(str);}
 function isDate(str){if(!/\d{4}(\.|\/|\-)\d{2}(\.|\/|\-)\d{2}/.test(str)){return false;}var r=str.match(/\d{1,4}/g);if(r==null){return false;};var d= new Date(r[0], r[1]-1, r[2]);return (d.getFullYear()==r[0]&&(d.getMonth()+1)==r[1]&&d.getDate()==r[2]);}
 //是中文，字母，横线，数字，下划线
 function isAllname(str){var patrn=/^[\u4E00-\u9FA5\uF900-\uFA2D\w-]+$/;return patrn.test(str);}
+
+function checkLink(link){
+	var strRegex = '^((https|http|ftp|rtsp|mms)?://)'
+                + '?(([0-9a-z_!~*\'().&=+$%-]+: )?[0-9a-z_!~*\'().&=+$%-]+@)?' //ftp的user@
+                + '(([0-9]{1,3}.){3}[0-9]{1,3}' // IP形式的URL- 199.194.52.184
+                + '|' // 允许IP和DOMAIN（域名）
+                + '([0-9a-z_!~*\'()-]+.)*' // 域名- www.
+                + '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' // 二级域名
+                + '[a-z]{2,6})' // first level domain- .com or .museum
+                + '(:[0-9]{1,4})?' // 端口- :80
+                + '((/?)|' // a slash isn't required if there is no file name
+                + '(/[0-9a-z_!~*\'().;?:@&=+$,%#-]+)+/?)$';
+	var reg = new RegExp(strRegex);
+	if(!reg.test(link.toLowerCase())){
+		return false;
+	}
+	return true;
+}
